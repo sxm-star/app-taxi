c803bf7aa205d6ac07fc33b034c60c68f1570223
xuanmings <18720987043@139.com>
2018-04-30 01:06:48 +0800

    .

---
+++ app/src/main/java/com/songxm/taxi/main/MainActivity.java
@@ -0,0 +1,8 @@
+package com.songxm.taxi.main;
+
+/**
+ * @author sxm
+ */
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/songxm/taxi/splash/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.songxm.taxi.splash;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.songxm.taxi.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            final AnimatedVectorDrawable anim1 = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            final ImageView logo = ((ImageView) findViewById(R.id.logo));
+            logo.setImageDrawable(anim1);
+            anim1.start();
+        }
+        /**
+         * 延时 3 秒然后跳转到 main 页面
+         */
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//                finish();
+//            }
+//        }, 3000) ;
+    }
+}
